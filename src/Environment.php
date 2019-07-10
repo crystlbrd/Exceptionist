aa22e37143686750fc3270b721484ccf7202d27b
crystlbrd <e.bitkov@hotmail.de>
2019-07-10 21:25:22 +0200

    adding some comments

--- src/Environment.php
+++ src/Environment.php
@@ -8,9 +8,18 @@ namespace crystlbrd\Exceptionist;
  */
 class Environment
 {
+    // Debug info
     const E_LEVEL_DEBUG = 0;
+
+    // An info for the developer, but not an actual error
     const E_LEVEL_INFO = 1;
+
+    // An error, but the program can actually go on
     const E_LEVEL_WARNING = 2;
+
+    // An error breaking the script from going on
     const E_LEVEL_ERROR = 3;
+
+    // Ignores any exceptions
     const E_LEVEL_NONE = 4;
 }
