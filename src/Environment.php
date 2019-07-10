431b9488e8bf74a49c1378fed3f4970d71e018d4
crystlbrd <e.bitkov@hotmail.de>
2019-07-10 21:22:18 +0200

    Implementing functionality

--- src/Environment.php
+++ src/Environment.php
@@ -2,6 +2,15 @@
 
 namespace crystlbrd\Exceptionist;
 
+/**
+ * Defines environment variables
+ * @package crystlbrd\Exceptionist
+ */
 class Environment
 {
+    const E_LEVEL_DEBUG = 0;
+    const E_LEVEL_INFO = 1;
+    const E_LEVEL_WARNING = 2;
+    const E_LEVEL_ERROR = 3;
+    const E_LEVEL_NONE = 4;
 }
--- src/ExceptionistTrait.php
+++ src/ExceptionistTrait.php
@@ -2,6 +2,73 @@
 
 namespace crystlbrd\Exceptionist;
 
+use Exception;
+
+/**
+ * Provides exception logging mechanisms
+ * @package crystlbrd\Exceptionist
+ */
 trait ExceptionistTrait
 {
+    /**
+     * @var array saves exceptions internally
+     */
+    private $ExeptionistLog = [];
+
+    /**
+     * @var int level of exception reporting
+     */
+    private $ExceptionistReportingLevel = Environment::E_LEVEL_ERROR;
+
+    /**
+     * Logs an exception and throws it, if its level is high enough
+     * @param Exception $exception
+     * @param int $level level of exception
+     * @throws Exception
+     */
+    private function log(Exception $exception, int $level = Environment::E_LEVEL_DEBUG): void
+    {
+        // Get trace info of previous function
+        $trace = debug_backtrace();
+        $debugInfo = $trace[1];
+
+        // Always log the exception
+        $this->ExeptionistLog[] = [
+            'function' => $debugInfo['function'],
+            'line' => $debugInfo['line'],
+            'level' => $level,
+            'exception' => $exception
+        ];
+
+        // throw exception, if its level is high enough
+        if ($level >= $this->ExceptionistReportingLevel) {
+            throw $exception;
+        }
+    }
+
+    /**
+     * Gets all internally saved logs higher or equal than the given level
+     * @param int $level
+     * @return array
+     */
+    private function getLogs(int $level = Environment::E_LEVEL_DEBUG): array
+    {
+        $result = [];
+        foreach ($this->ExeptionistLog as $log) {
+            if ($log['level'] >= $level) {
+                $result[] = $log;
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Gets the last logged exception
+     * @return array
+     */
+    private function getLastLog(): array
+    {
+        return $this->ExeptionistLog[(count($this->ExeptionistLog) - 1)];
+    }
 }
